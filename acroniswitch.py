1674b310c7b5760fec53c2221b3a99a734f74827
AntonyShklyar <95246873+AntonyShklyar@users.noreply.github.com>
2022-04-09 15:04:07 +0300

    Update acroniswitch.py

--- acroniswitch.py
+++ acroniswitch.py
@@ -29,16 +29,25 @@ def networkavailable(var, g):
                 return 0
 '''
 Editable parameters:
---domain
+--segment
 The data type is a dictionary.
-Data - domain: list of IP addresses of domain stores
+Data - domain: list of IP addresses Acronisserver and segments
+--codid
+The data type is a dictionary.
+Data - Data Center ID: IP Acronis server
 ''''
-domain={'ac.com':['10.111.15.56', '10.111.15.65', '10.111.15.77'],'vp.com':['10.111.16.56', '10.111.16.65', '10.111.16.77'],'in.com':['10.111.17.56', '10.111.17.65', '10.111.17.77']}
-IP=[]
+segment={'ac.com':['10.111.15.56', '10.111.15.65', '10.111.15.77'],'vp.com':['10.111.16.56', '10.111.16.65', '10.111.16.77'],'in.com':['10.111.17.56', '10.111.17.65', '10.111.17.77']}
+codid={'01':['10.111.15.56', '10.111.16.56', '10.111.17.56'],'02':['10.111.15.65', '10.111.16.65', '10.111.17.77'],'03':['10.111.15.77', '10.111.16.77', '10.111.17.77']}
+acronis=[]
 logs()
-for x, y in domain.items():
+for x, y in segment.items():
 	if x in a:
-		IP.append(y)
+		acronis.append(y)
+for x, y in codid.items():
+	for z in y:
+		if z in acronis:
+			acronis.remove(z)
+			acronis.insert(0, z)
 for var, g in enumerate(IP, 1):
         b=networkavailable(var, g)
         if b==0:
@@ -54,7 +63,7 @@ for var, g in enumerate(IP, 1):
 			if subprocess.call(["/usr/lib/Acronis/RegisterAgentTool/RegisterAgent", "-o", "register", "-a", g]) == 0: os.system('echo $(date +"%Y%m%d-%H%M%S") Acronis Server change operation '+g+' Successful >> /var/log/acronis.log'); exit()
 			else:
 				os.system('echo $(date +"%Y%m%d-%H%M%S") Acronis Server change operation on '+g+' Unsuccessful >> /var/log/acronis.log'); exit()
-	elif var != len(IP):
+	elif var != len(acronis):
 		continue
         else:
                 exit()
